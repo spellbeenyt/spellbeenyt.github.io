f26e41fdd766b60eb5a35bd093f8a230019fa59a
spellbeenyt <nikedino121@gmail.com>
2025-12-08 20:57:40 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,41 +19,39 @@ export const metadata: Metadata = {
 };
 
 const videoGameSchema = {
-  "@context": "https://schema.org",
-  "@type": "VideoGame",
-  "name": "Spell Bee",
-  "description": "A spelling game for everyone",
-  "url": "https://spellbeenyt.github.io",
-  "applicationCategory": "Game",
-  "gamePlatform": "Web Browser",
-  "genre": ["Educational", "Word Game", "Spelling", "Puzzle"],
-  "keywords": "spell bee, spelling bee, guess the word game, word guess, spelling game, vocabulary, word puzzle, educational game, language game, spelling practice",
-  "operatingSystem": "Web",
-  "author": {
-    "@type": "Person",
-    "name": "spell bee"
-  },
-  "offers": {
-    "@type": "Offer",
-    "price": "0",
-    "priceCurrency": "USD"
-  },
-  "playMode": "SinglePlayer",
-  "gameLocation": "Online",
-  "audience": {
-    "@type": "PeopleAudience",
-    "suggestedMinAge": "6",
-    "suggestedMaxAge": "99"
-  },
-  "softwareRequirements": "Web browser with JavaScript enabled",
-  "educationalUse": "Spelling practice, vocabulary building, language learning",
-  "learningResourceType": "Game",
-  "typicalAgeRange": "6-99",
-  "timeRequired": "PT5M",
-  "interactivityType": "active",
-  "isAccessibleForFree": true
-}
-		}	
+	"@context": "https://schema.org",
+	"@type": "VideoGame",
+	"name": "Spell Bee",
+	"description": "A spelling game for everyone",
+	"url": "https://spellbeenyt.github.io",
+	"applicationCategory": "Game",
+	"gamePlatform": "Web Browser",
+	"genre": ["Educational", "Word Game", "Spelling", "Puzzle"],
+	"keywords": "spell bee, spelling bee, guess the word game, word guess, spelling game, vocabulary, word puzzle, educational game, language game, spelling practice",
+	"operatingSystem": "Web",
+	"author": {
+		"@type": "Person",
+		"name": "spell bee"
+	},
+	"offers": {
+		"@type": "Offer",
+		"price": "0",
+		"priceCurrency": "USD"
+	},
+	"playMode": "SinglePlayer",
+	"gameLocation": "Online",
+	"audience": {
+		"@type": "PeopleAudience",
+		"suggestedMinAge": "6",
+		"suggestedMaxAge": "99"
+	},
+	"softwareRequirements": "Web browser with JavaScript enabled",
+	"educationalUse": "Spelling practice, vocabulary building, language learning",
+	"learningResourceType": "Game",
+	"typicalAgeRange": "6-99",
+	"timeRequired": "PT5M",
+	"interactivityType": "active",
+	"isAccessibleForFree": true
 };
 
 export default function RootLayout({
@@ -68,9 +66,6 @@ export default function RootLayout({
 			lang='tr'
 			maximum-scale='1'>
 			<head>				
-
-				
-				
 				{/* Schema Markup */}
 				<script
 					type="application/ld+json"
@@ -79,7 +74,7 @@ export default function RootLayout({
 				
 				{/* Google Analytics (GA4) */}
 				<Script
-					src="https://www.googletagmanager.com/gtag/js?id=G-B86Q3W5JM6" // ← REPLACE WITH YOUR GA4 ID
+					src="https://www.googletagmanager.com/gtag/js?id=G-B86Q3W5JM6"
 					strategy="afterInteractive"
 				/>
 				<Script id="google-analytics" strategy="afterInteractive">
@@ -87,7 +82,7 @@ export default function RootLayout({
 						window.dataLayer = window.dataLayer || [];
 						function gtag(){dataLayer.push(arguments);}
 						gtag('js', new Date());
-						gtag('config', 'G-B86Q3W5JM6'); // ← REPLACE WITH YOUR GA4 ID
+						gtag('config', 'G-B86Q3W5JM6');
 					`}
 				</Script>
 			</head>
